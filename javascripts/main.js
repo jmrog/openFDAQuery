fe7072c6e2e3c3056664c716fc7314452e106b1b
Jason Rogers <jason@ascendiv.com>
2015-06-21 15:23:29 -0400

    enhancements to searches (supports AND/OR, exact/inexact matching, skipping), loading indicator, and small cleanup

--- javascripts/main.js
+++ javascripts/main.js
@@ -14,22 +14,27 @@
 
         // begin "Add Search Item" button click handler
         $('#search-item-fieldset').on('click', 'button#add-search-term', function() {
-            var searchItemClone = $('div.search-item').last().clone(),
-                spanSearchItemNum = searchItemClone.find('span.search-item-num');
+            var searchItemClone = $('div.search-item').first().clone(),
+                spanSearchItemNum = $('div.search-item').last().find('span.search-item-num');
                 numSearchItems = parseInt(spanSearchItemNum.text(), 10),
-                searchItemNum = numSearchItems + 1;
+                searchItemNum = numSearchItems + 1,
+                joiningTermsTemplate = '<span>Join searches with... ' +
+                        '<label><input type="radio" name="joining-term-' + numSearchItems + '" value="AND" checked="checked" /> AND</label>' +
+                        '&nbsp;<label><input type="radio" name="joining-term-' + numSearchItems + '" value="OR" /> OR</label></span>';
 
             // Increment the values for id/name attributes on the clone elements
-            searchItemClone.find('[id*="-' + numSearchItems + '"]').each(function() {
+            searchItemClone.find('[id*="-1"],[name*="-1"]').each(function() {
                 var el = $(this);
-                el.prop('id', el.prop('id').replace('-' + numSearchItems, '-' + searchItemNum));
                 if (el.is('input')) {
-                    el.prop('name', el.prop('name').replace('-' + numSearchItems, '-' + searchItemNum));
+                    el.prop('name', el.prop('name').replace('-1', '-' + searchItemNum));
+                } else {
+                    el.prop('id', el.prop('id').replace('-1', '-' + searchItemNum));
                 }
             });
 
-            spanSearchItemNum.text(searchItemNum);
-            searchItemClone.find('input').val('');
+            searchItemClone.find('span.search-item-num').text(searchItemNum);
+            searchItemClone.find('input[type="text"]').val('');
+            searchItemClone.prepend($('<div>').prop('class', 'joining-terms').html(joiningTermsTemplate));
             searchItemClone.insertBefore('button#add-search-term');
         }); // end "Add Search Item" button click handler
 
@@ -46,7 +51,7 @@
 
         // begin "Submit" click handler
         $('#query-form').on('submit', function(evt) {
-            var dataSet, subDataSet, searchItems, fullSearch, baseURL, apiKey, limit;
+            var dataSet, subDataSet, searchItems, fullSearch, baseURL, apiKey, limit, skip;
             var el = $(this);
 
             evt.preventDefault();
@@ -57,6 +62,10 @@
             apiKey = 'MzPnC5U3ET5CKXrmWrfBxh7YSIhSmZVw2Ao2upAq';
             dataSet = el.find('input[name="data-set"]:checked').val();
             subDataSet = el.find('input[name="sub-data-' + dataSet + '"]:checked').val();
+            limit = parseInt($('input[name="limit"]').val(), 10) || 25;
+            skip = parseInt($('input[name="skip"]').val(), 10) || 0;
+            baseURL = 'https://api.fda.gov/' + dataSet + '/' + subDataSet + '.json?api_key=' + apiKey +
+                      '&limit=' + limit + '&skip=' + skip + '&search=';
 
             // construct array of searchItems as objects with `field` and `term` properties
             // Right now, this seems easier than parsing a serialized string of form data, but that may
@@ -81,29 +90,32 @@
             }
 
             fullSearch = searchItems.reduce(function(searchString, item, idx) {
+                var joiningTerm = '',
+                    exactMatch = (el.find('input[name="exact-match-' + (idx + 1) + '"]:checked').val() === 'yes' ||
+                                  /^\[.*\]$/.test(item.term));
+
                 if (item.field) {
                     searchString += item.field + ':';
                 }
                 if (item.term) {
-                    searchString += '"' + item.term + '"';
+                    searchString += exactMatch ? '"' + item.term + '"' : item.term;
                 }
                 if (idx < (searchItems.length - 1)) {
                     // There are additional search items coming
-                    searchString += '+AND+';
+                    joiningTerm = el.find('input[name="joining-term-' + (idx + 1) + '"]:checked').val();
+                    searchString += joiningTerm === 'AND' ? '+AND+' : '+';
                 }
                 return searchString;
             }, '');
 
-            limit = parseInt($('input[name="limit"]').val(), 10) || 25;
-
-            baseURL = 'https://api.fda.gov/' + dataSet + '/' + subDataSet + '.json?api_key=' + apiKey +
-                      '&limit=' + limit + '&search=';
-
+            $('.loading').removeClass('hidden');
             $.getJSON(baseURL + fullSearch)
                 .done(function(data) {
+                    $('.loading').addClass('hidden');
                     $('#results').find('pre').text(JSON.stringify(data, null, 4));
                 })
                 .fail(function(xhr, msg, error) {
+                    $('.loading').addClass('hidden');
                     if (xhr.status === 404) {
                         $('#results').find('pre').text('No results found that match your criteria.');
                     } else {
